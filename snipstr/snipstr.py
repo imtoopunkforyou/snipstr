96511a19f01fc6190d5bae9bae201ba0b9b5bebe
Timur Valiev <cptchunk@yandex.ru>
2025-11-09 20:31:57 +0500

    Refactor SnipStr class hierarchy with base classes and improved architecture (#5)

--- snipstr/snipstr.py
+++ snipstr/snipstr.py
@@ -1,46 +1,45 @@
-from typing import Annotated, Any, TypeVar, final
+from typing import final
 
-from snipstr.base import ComparableByLength
+from typing_extensions import Self
+
+from snipstr.base import (
+    BuilderSnipStr,
+    ComparableSnipStr,
+    HashedSnipStr,
+)
 from snipstr.constants import Sides
 from snipstr.errors import (
     SnipSideError,
     SnipSizeIsNotIntError,
     SnipSizeIsNotPositiveIntError,
 )
-
-SelfSnipStr = TypeVar('SelfSnipStr', bound='SnipStr')
-SnipStrInstance = TypeVar('SnipStrInstance', bound='SnipStr')
-
-PositiveInt = Annotated[int, 'An integer that is greater than 0.']
+from snipstr.types import PositiveInt
 
 
 @final
-class SnipStr(ComparableByLength):
+class SnipStr(ComparableSnipStr, HashedSnipStr, BuilderSnipStr):
     """Example:
     >>> TODO = 'TODO'
     """
 
-    def __init__(self, source: Any) -> None:
-        self._source = source
-        self._max_lenght = len(str(source))
-        self._side = Sides.RIGHT.value
-        self._replacement_symbol = ''
+    __slots__ = (
+        '_lenght',
+        '_replacement_symbol',
+        '_side',
+        '_source',
+    )
 
-    @property
-    def source(self) -> Any:
-        return self._source
-
-    def snip_to(self, size: PositiveInt, /) -> SelfSnipStr:
+    def snip_to(self, size: PositiveInt, /) -> Self:
         if not isinstance(size, int):
             raise SnipSizeIsNotIntError(size)
         if size <= 0:
             raise SnipSizeIsNotPositiveIntError(size)
 
-        self._max_lenght = size
+        self._lenght = size
 
         return self
 
-    def by_side(self, side: Sides, /) -> SelfSnipStr:
+    def by_side(self, side: Sides, /) -> Self:
         if side not in Sides:
             raise SnipSideError(side)
 
@@ -52,61 +51,16 @@ class SnipStr(ComparableByLength):
         self,
         symbol: str | None = None,
         /,
-    ) -> SelfSnipStr:
-        symbol = '...' if symbol is None else str(symbol)
+    ) -> Self:
+        default = '...'
+        symbol = default if symbol is None else str(symbol)
+
         self._replacement_symbol = symbol
 
         return self
 
-    def _cut_back(self, current: str) -> str:
-        if self._side == 'right':
-            current = current[: self._max_lenght]
-        elif self._side == 'left':
-            current = current[self._max_lenght :]
-
-        return current
-
-    def _add_replacement_symbol(self, current: str) -> str:
-        if self._replacement_symbol:
-            symbol_lenght = len(self._replacement_symbol)
-            if self._side == Sides.RIGHT.value:
-                current = current[:-symbol_lenght] + self._replacement_symbol
-            elif self._side == Sides.LEFT.value:
-                current = self._replacement_symbol + current[symbol_lenght:]
-
-        return current
-
     def __len__(self) -> int:
-        return self._max_lenght
+        return self._lenght
 
     def __str__(self) -> str:
-        current = str(self._source)
-
-        current = self._cut_back(current)
-        current = self._add_ellipsis(current)
-
-        return current
-
-    def __hash__(self) -> int:
-        return hash(
-            self._source,
-            self._max_lenght,
-            self._side,
-            self._replacement_symbol,
-        )
-
-    def __eq__(self, other: SnipStrInstance | Any) -> bool:
-        if not isinstance(other, SnipStr):
-            return NotImplemented
-
-        return all(
-            (
-                (self._source == other._source),
-                (self._max_lenght == other._max_lenght),
-                (self._side == other._side),
-                (self._replacement_symbol == other._replacement_symbol),
-            ),
-        )
-
-    def _total_tength(self) -> int:
-        return self._max_lenght + len(self._replacement_symbol)
+        return self._build(str(self._source))
