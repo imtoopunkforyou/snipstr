9ddcc12524f83e4ee3d8813cd0304c265ce93c85
Timur Valiev <cptchunk@yandex.ru>
2025-11-12 08:25:28 +0300

    fix test_create_snipstr_without_sides_enum

--- tests/test_snipstr.py
+++ tests/test_snipstr.py
@@ -22,9 +22,14 @@ def test_snipstr_w_replacement_symbol(long_text):
 def test_create_snipstr_without_sides_enum(long_text):
     max_len = 10
     s = SnipStr(long_text)
+
     s.by_side('left').with_replacement_symbol().snip_to(max_len)
     assert len(str(s)) == max_len
     assert str(s).startswith('...')
+
+    s.by_side('right').with_replacement_symbol().snip_to(max_len)
+    assert len(str(s)) == max_len
+    assert str(s).endswith('...')
 
 
 def test_snipstr_w_default_right_side(long_text):
