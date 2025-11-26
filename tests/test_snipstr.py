5104f8964839411e0483130d2919d8caffa8d3e6
Timur Valiev <cptchunk@yandex.ru>
2025-11-26 21:07:48 +0300

    refact tests

--- tests/test_snipstr.py
+++ tests/test_snipstr.py
@@ -1,13 +1,15 @@
+import pytest
+
 from snipstr.constants import Sides
 from snipstr.snipstr import SnipStr
 
 
-def test_snipstr(long_text):
+@pytest.mark.parametrize('side', [Sides.LEFT, Sides.RIGHT])
+def test_snipstr(long_text, side):
     max_len = 10
     s = SnipStr(long_text)
-    s.snip_to(max_len).by_side(Sides.LEFT).with_replacement_symbol()
+    s.snip_to(max_len).by_side(side).with_replacement_symbol()
 
-    assert str(s).startswith('...')
     assert len(str(s)) < len(long_text)
     assert len(str(s)) == max_len
     assert len(s) == len(str(s))
@@ -16,24 +18,25 @@ def test_snipstr(long_text):
 def test_snipstr_w_replacement_symbol(long_text):
     symbol = '<...>'
     s = SnipStr(long_text).with_replacement_symbol(symbol)
+
     assert symbol in str(s)
 
 
-def test_create_snipstr_without_sides_enum(long_text):
+@pytest.mark.parametrize('side', [Sides.LEFT.value, Sides.RIGHT.value])
+def test_create_snipstr_without_sides_enum(long_text, side):
     max_len = 10
     s = SnipStr(long_text)
 
-    s.by_side('left').with_replacement_symbol().snip_to(max_len)
+    s.by_side(side).with_replacement_symbol().snip_to(max_len)
     assert len(str(s)) == max_len
-    assert str(s).startswith('...')
 
-    s.by_side('right').with_replacement_symbol().snip_to(max_len)
+    s.by_side(side).with_replacement_symbol().snip_to(max_len)
     assert len(str(s)) == max_len
-    assert str(s).endswith('...')
 
 
-def test_snipstr_w_default_right_side(long_text):
+def test_snipstr_w_default_right_side_and_default_replacement_symbol(long_text):
     max_len = 10
     s = SnipStr(long_text).snip_to(max_len).with_replacement_symbol()
+
     assert len(str(s)) == max_len
     assert str(s).endswith('...')
